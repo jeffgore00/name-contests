1f0189f1037518db10b3e7124ccdb38a5c53f710
Jeff Gore <jgore00@gmail.com>
2021-04-02 06:42:23 -0400

    chore: more sql changes

--- database/test-pg-data.sql
+++ database/test-pg-data.sql
@@ -1,22 +1,35 @@
-drop table if exists teams CASCADE;
+
+ drop table if exists teams CASCADE;
 drop table if exists players CASCADE;
 
-CREATE TABLE teams (
+ CREATE TABLE teams (
   id serial primary key,
   city varchar(128) not null,
   name varchar(128) not null,
   created_at timestamp not null default current_timestamp
 );
 
-
-CREATE TABLE players (
+CREATE TABLE people (
   id serial primary key,
   first_name varchar(128) not null,
   last_name varchar(128) not null,
-  jersey_number varchar(128) not null,
-  active boolean not null,
-  team_id integer REFERENCES teams (id),
   created_at timestamp not null default current_timestamp
+);
+
+
+CREATE TABLE players (
+  id serial primary key,
+  person_id integer REFERENCES people (id),
+  active boolean not null,
+  created_at timestamp not null default current_timestamp
+);
+
+CREATE TABLE players_teams (
+  player_id integer REFERENCES players (id),
+  team_id integer REFERENCES teams (id),
+  jersey_number varchar(128) not null,
+  start_date timestamp not null default current_timestamp,
+  end_date timestamp default current_timestamp
 );
 
 
@@ -25,10 +38,27 @@ VALUES
 ('Lakers','Los Angeles'),
 ('Pelicans','New Orleans');
 
-
-INSERT INTO players (first_name, last_name, jersey_number, active, team_id)
+INSERT INTO people (first_name, last_name)
 VALUES
-('LeBron', 'James', 23, true, 1),
-('Anthony', 'Davis', 6, true, 1),
-('Danny', 'Green', 33, true, 1),
-('Zion', 'Williamson', 69, true, 2);
+('LeBron', 'James'),
+('Anthony', 'Davis'),
+('Danny', 'Green'),
+('Zion', 'Williamson');
+
+
+INSERT INTO players (person_id, active)
+VALUES
+(1, true),
+(2, true),
+(3, true),
+(4, true);
+
+INSERT INTO players_teams (player_id, team_id, jersey_number)
+VALUES
+(1, 1, 23),
+(2, 1, 6),
+(3, 1, 33),
+(4, 2, 69);
+
+
+
